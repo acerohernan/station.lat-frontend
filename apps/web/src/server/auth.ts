3963255b3fbe69758445a960d5626768765826b1
acerohernan <contacto.acero.hernan@gmail.com>
2023-04-03 18:06:39 -0500

    build(web): Adding Next Auth and setupping prisma

---
+++ apps/web/src/server/auth.ts
@@ -0,0 +1,46 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+import { GetServerSidePropsContext } from "next";
+import { DefaultSession, NextAuthOptions, getServerSession } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { env } from "process";
+
+/* Allow us to add custom object and keep tyoe safety. */
+declare module "next-auth" {
+  interface Session extends DefaultSession {
+    user: {
+      id: string;
+      // ...other properties
+    } & DefaultSession["user"];
+  }
+}
+
+const prisma = new PrismaClient();
+
+/* Next Auth configuration */
+export const authOptions: NextAuthOptions = {
+  callbacks: {
+    session({ session, user }) {
+      if (session.user) {
+        // eslint-disable-next-line no-param-reassign
+        session.user.id = user.id;
+        // session.user.role = user.role; <-- put other properties on the session here
+      }
+
+      return session;
+    },
+  },
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: env.GOOGLE_CLIENT_ID as string,
+      clientSecret: env.GOOGLE_CLIENT_ID as string,
+    }),
+  ],
+};
+
+/* Wrapper for 'getServerSideSession' */
+export const getServerAuthSession = (ctx: {
+    req: GetServerSidePropsContext["req"],
+    res: GetServerSidePropsContext["res"]
+}) => getServerSession(ctx.req, ctx.res, authOptions);
